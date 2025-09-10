3827064081b1d31e9e1b35f93fcd573d1ad805b3
Hail <jyealc@gmail.com>
2025-09-10 14:15:47 +0800

    .13

--- overlay.user.js
+++ overlay.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Hail's OP
 // @namespace    http://tampermonkey.net/
-// @version      2.8.12
+// @version      2.8.13
 // @author       shinkonet (Altered by Hail)
 // @match        https://wplace.live/*
 // @license      GPLv3
