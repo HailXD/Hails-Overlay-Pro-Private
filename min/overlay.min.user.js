f34210f8f8ddfb8938af58bf660731c98b12ded6
HailXD <77497878+HailXD@users.noreply.github.com>
2025-09-10 11:54:54 +0000

    bot: Add minified userscript

--- min/overlay.min.user.js
+++ min/overlay.min.user.js
@@ -14,8 +14,8 @@
 // @grant        unsafeWindow
 // @connect      *
 // @run-at       document-start
-// @updateURL    https://github.com/HailXD/Hails-Overlay-Pro/raw/refs/heads/main/overlay.min.user.js
-// @downloadURL  https://github.com/HailXD/Hails-Overlay-Pro/raw/refs/heads/main/overlay.min.user.js
+// @updateURL    https://github.com/HailXD/Hails-Overlay-Pro/raw/refs/heads/main/min/overlay.min.user.js
+// @downloadURL  https://github.com/HailXD/Hails-Overlay-Pro/raw/refs/heads/main/min/overlay.min.user.js
 // ==/UserScript==
 @endpreserve */
 
